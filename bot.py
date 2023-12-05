c0af2b27075ce36635350618b689f5e16c6d89c8
m30wm30www1amak1tten <97727534+methadonetrip@users.noreply.github.com>
2023-12-05 17:48:01 +0300

    added normal button names to bot.py and updated keyboard row

--- bot.py
+++ bot.py
@@ -20,34 +20,38 @@ dp = Dispatcher(bot)
 
 # Define custom keyboard
 keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-subscribe_button = KeyboardButton('/subscribe')
-unsubscribe_button = KeyboardButton('/unsubscribe')
-list_button = KeyboardButton('/list')
-add_channel_button = KeyboardButton('/add_channel')
-add_keyword_button = KeyboardButton('/add_keyword')
-remove_channel_button = KeyboardButton('/remove_channel')
-remove_keyword_button = KeyboardButton('/remove_keyword')
+subscribe_button = KeyboardButton('subscribe')
+unsubscribe_button = KeyboardButton('unsubscribe')
+list_button = KeyboardButton('list')
+add_channel_button = KeyboardButton('add_channel') # do
+add_keyword_button = KeyboardButton('add_keyword') # not 
+remove_channel_button = KeyboardButton('remove_channel') # work
+remove_keyword_button = KeyboardButton('remove_keyword') # normally
+artem_trahni = KeyboardButton('Artem, trahni plz po bratski))')
 keyboard.row(subscribe_button, unsubscribe_button, list_button)
-keyboard.row(add_channel_button, add_keyword_button, remove_channel_button, remove_keyword_button)
+keyboard.row(add_channel_button, add_keyword_button)
+keyboard.row(remove_channel_button, remove_keyword_button)
 
 # Command to start the bot
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.reply("Bot is running. Use the keyboard below:", reply_markup=keyboard)
 
+
 # Command to subscribe to keyword notifications
 @dp.message_handler(commands=['subscribe'])
 async def subscribe(message: types.Message):
     user_id = message.from_user.id
     SUBSCRIBED_USERS.add(user_id)
-    await message.reply("You are subscribed. You will receive messages with keywords.", reply_markup=keyboard)
+    await message.reply("You are subscribed. You will receive messages with your keywords.", reply_markup=keyboard)
 
 # Command to unsubscribe from keyword notifications
 @dp.message_handler(commands=['unsubscribe'])
 async def unsubscribe(message: types.Message):
     user_id = message.from_user.id
     SUBSCRIBED_USERS.discard(user_id)
-    await message.reply("You are unsubscribed. You will no longer receive messages with keywords.", reply_markup=keyboard)
+    await message.reply("You are unsubscribed. You will no longer receive messages with your keywords.", \
+                        reply_markup=keyboard)
 
 # Command to list currently monitored channels and keywords
 @dp.message_handler(commands=['list'])
@@ -72,7 +76,7 @@ async def handle_messages(message: types.Message):
         # Forward the message to all subscribed users
         for user_id in SUBSCRIBED_USERS:
             await bot.forward_message(user_id, chat_id, message.message_id)
-            await bot.send_message(user_id, "New message with keyword received!")
+            await bot.send_message(user_id, "New message with keyword received.")
 
 
 
@@ -92,11 +96,12 @@ async def remove_channel(message: types.Message):
     try:
         channel = message.text.split(' ', 1)[1]
         CHANNELS.remove(channel)
-        await message.reply(f"Channel {channel} removed successfully.", reply_markup=keyboard)
     except IndexError:
         await message.reply("Please provide a channel name after the command.", reply_markup=keyboard)
     except ValueError:
         await message.reply(f"Channel {channel} not found in the list.", reply_markup=keyboard)
+    else:
+        await message.reply(f"Channel {channel} removed нахуй successfully.", reply_markup=keyboard) # check if True
 
 # Command to add a keyword
 @dp.message_handler(commands=['add_keyword'])
@@ -135,7 +140,7 @@ async def handle_messages(message: types.Message):
         # Forward the message to all subscribed users
         for user_id in SUBSCRIBED_USERS:
             await bot.forward_message(user_id, chat_id, message.message_id)
-            await bot.send_message(user_id, "New message with keyword received!")
+            await bot.send_message(user_id, "New message with keyword received.")
 
 if __name__ == '__main__':
     # Start the bot
