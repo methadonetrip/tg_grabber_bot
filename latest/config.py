d1f9345165e76321f590f3c6a18102ec1ac51377
Rootyz <142735638+Rootyz@users.noreply.github.com>
2023-12-18 04:12:16 +0300

    Update config.py

--- latest/config.py
+++ latest/config.py
@@ -5,6 +5,6 @@ password = "Po25qwe!"
 db_name = "usersinfo"
 
 #API
-api_id = '27149831'
-api_hash = '0f2f5320a88e77d90f2cd470ce8353aa' 
-bot_token = '6776409787:AAEOHAIOpKMdTK9dFhQVJRLB09c3lqd5-HQ'
+api_id = ''
+api_hash = '' 
+bot_token = ''
